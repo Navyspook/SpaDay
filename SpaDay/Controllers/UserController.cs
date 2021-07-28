f69164bbc79bce6e113ba280f5fcad5cfb0f6d12
Justin Joseph <jmjoseph20@hotmail.com>
2021-07-28 12:35:50 -0400

    start of ch 15 studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //[Route("/User/Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/User/Add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            if (verify == newUser.Password)
+            {
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                //ViewBag.dt = newUser.DateTime;
+                return View("~/Views/User/Index.cshtml");
+            }
+            else
+            {
+                ViewBag.email = newUser.Email;
+                ViewBag.username = newUser.Username;
+                ViewBag.error = "Your passwords did not match. Please try again.";
+                return View("~/Views/User/Add.cshtml");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        //public string DateTime = 
+        public User()
+        {
+
+        }
+
+        public User(string u, string e, string p)
+        {
+            Username = u;
+            Email = e;
+            Password = p;
+        }
+
+    }
+}
